172fe1c32d705fe9831f07b7ee9c0059c2e13794
MadugodageGT <gtharaka784@gmail.com>
2025-11-23 22:30:30 +0530

    adding uniforms to animate colors

--- main.cpp
+++ main.cpp
@@ -12,9 +12,10 @@ const char* vertexShaderSource = "#version 330 core\n"
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"uniform vec4 ourColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = ourColor;\n"
 "}\0";
 
 
@@ -88,14 +89,6 @@ int main() {
 
 	//_______________________________________________________________________________________________
 
-	
-	
-
-	//_______________________________________________________________________________________________
-
-
-
-
 
 	//shaders
 	//_______________________________________________________________________________________________
@@ -156,6 +149,22 @@ int main() {
 	//_______________________________________________________________________________________________
 
 
+	//Uniforms
+
+	//float timeValue = glfwGetTime();
+	//float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+	//int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+	//glUseProgram(shaderProgram);
+	//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+
+	//______________________________________________________________________________________________
+
+
+
+
+
+
 
 	//main rendering loop
 	while (!glfwWindowShouldClose(window)) {
@@ -164,6 +173,11 @@ int main() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+
+		float timeValue = glfwGetTime();
+		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
 
 		glUseProgram(shaderProgram);
