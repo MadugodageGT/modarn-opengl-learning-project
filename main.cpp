8fa0571d412b059c37b831c51b441018825eba36
MadugodageGT <gtharaka784@gmail.com>
2025-11-24 13:42:32 +0530

    feat/made the triangle with interpolating colors

--- main.cpp
+++ main.cpp
@@ -5,26 +5,29 @@
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"	
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"	ourColor = aColor;"
 "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"uniform vec4 ourColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = ourColor;\n"
+"   FragColor = vec4(ourColor, 1.0f);\n"
 "}\0";
 
 
 
 
 float vertices[] = {
-0.0f, 0.5f, 0.0f,  // top
-0.5f, -0.5f, 0.0f, // bottom right
--0.5f, -0.5f, 0.0f // bottom left
+0.0f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top
+0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,// bottom right
+-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left
 };
 
 unsigned int indices[] = {
@@ -82,8 +85,10 @@ int main() {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(1,3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
 
 	//_______________________________________________________________________________________________
 
@@ -154,13 +159,6 @@ int main() {
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
-
-		float timeValue = glfwGetTime();
-		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-
 
 		glUseProgram(shaderProgram);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
