18123318bf05f5f0c040c198ea4589677a7d6022
MadugodageGT <gtharaka784@gmail.com>
2025-11-25 21:23:24 +0530

    refactor: remove old uneccery shader declearations

--- main.cpp
+++ main.cpp
@@ -5,23 +5,6 @@
 #include "Shader.h"
 
 
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"	
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"uniform vec4 ourColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = ourColor;\n"
-"}\0";
-
-
-
 
 float vertices[] = {
 	-0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f, // bottom left vertex with color red
