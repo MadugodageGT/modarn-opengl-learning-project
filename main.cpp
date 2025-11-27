0d9228acfbae17ff98b1235cc62c98f5d9d298a3
MadugodageGT <gtharaka784@gmail.com>
2025-11-27 21:08:24 +0530

    flip the texture
    Adjust texture mixing ratio and flip image on load
    
    Changed mixing ratio in default.frag from 0.5 to 0.7 to make texture2 more prominent. Added stbi_set_flip_vertically_on_load(true) in main.cpp to ensure textures are loaded with the correct orientation for OpenGL.

--- main.cpp
+++ main.cpp
@@ -102,6 +102,7 @@ int main() {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true);
 	unsigned char* data = stbi_load("assets/textures/brick_texture.jpg", &width, &height, &nrChannels, 0);
 
 
